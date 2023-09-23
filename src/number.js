dca4b9b39be74986244d7b30d25bcbf1ea578bff
davodm <irdavidnet@gmail.com>
2023-09-23 17:42:36 +0100

    fix: funny way to give space in long suffix for number abbreviation

--- src/number.js
+++ src/number.js
@@ -1,11 +1,11 @@
 const SI_PREFIXES = [
   { value: 1, symbol: "", long: "" },
-  { value: 1e3, symbol: "K", long: "Thousand" },
-  { value: 1e6, symbol: "M", long: "Million" },
-  { value: 1e9, symbol: "B", long: "Billion" },
-  { value: 1e12, symbol: "t", long: "Trillion" },
-  { value: 1e15, symbol: "q", long: "Quadrillion" },
-  { value: 1e18, symbol: "Q", long: "Quintillion" },
+  { value: 1e3, symbol: "K", long: " Thousand" },
+  { value: 1e6, symbol: "M", long: " Million" },
+  { value: 1e9, symbol: "B", long: " Billion" },
+  { value: 1e12, symbol: "t", long: " Trillion" },
+  { value: 1e15, symbol: "q", long: " Quadrillion" },
+  { value: 1e18, symbol: "Q", long: " Quintillion" },
 ];
 
 /**
