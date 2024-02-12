3c7408739cfb1ea3192fede0e25ee01d5e3c61af
davodm <irdavidnet@gmail.com>
2024-02-12 20:32:41 +0000

    fix: User-agent added to bypass Cloudflare's bot protection

--- src/api.js
+++ src/api.js
@@ -17,6 +17,8 @@ async function request($method, $params = {}) {
       Language: "en",
       Authorization: "Bearer " + process.env.IRANCRYPTO_API_KEY,
       "Content-Type": "application/json",
+      // User-agent added to bypass Cloudflare's bot protection
+      "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36",
     },
     signal: AbortSignal.timeout(10000), //Timeout is in ms
   });
